a35bd388e64896d7da3d0add4eed90dd6012e626
Seong-Kyu Yun <reviday@naver.com>
2020-06-17 23:41:15 +0900

    Working on customer page.

--- client/src/components/Contents/CustomerInfo/index.js
+++ client/src/components/Contents/CustomerInfo/index.js
@@ -1,11 +1,34 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import moment from 'moment';
+
 import BorderButton from 'common/Button/BorderButton';
+import Table from 'common/Table';
+import Paging from 'common/Paging';
+
+import './index.css';
 
 const CustomerInfo = (props) => {
   
   const addCostomer = () => {
     console.log('btn click');
   };
+
+  // Customer Table Header Set
+  const customerHeaderSet = [
+    { field: 'id', text: '번호', sort: '' },
+    { field: 'name', text: '고객명', sort: '' },
+    { field: 'join_date', text: '가입 날짜', sort: '' },
+    { field: 'last_order_date', text: '최근 주문날짜', sort: '' }
+  ];
+
+  const tempData = [
+    { id: 1, name: 'temp1', join_date: '2010-10-10', last_order_date: '2010-10-10' },
+    { id: 2, name: 'temp1', join_date: '2010-10-10', last_order_date: '2010-10-10' },
+    { id: 3, name: 'temp1', join_date: '2010-10-10', last_order_date: '2010-10-10' },
+    { id: 4, name: 'temp1', join_date: '2010-10-10', last_order_date: '2010-10-10' },
+    { id: 5, name: 'temp1', join_date: '2010-10-10', last_order_date: '2010-10-10' }
+  ];
     
   return (
     <React.Fragment>
@@ -26,9 +49,35 @@ const CustomerInfo = (props) => {
             </div>
           </div>
         </div>
-        <div className="ct_box">
-          content
+        <div className="ct_flex">
+          <div className="flex_box">
+            <div className="ct_box">
+              <div className="rows" style={{ width: 'calc(100% - 100px)' }}>
+                <div className="_rt">
+                  총 고객: 00명
+                </div>
+              </div>
+              <Table
+                headerSet={customerHeaderSet}
+                data={tempData}
+                recordLimit={5}
+                tableStyle={{ marginTop: '10px' }}
+              />
+            </div>
+          </div>
+          <div className="flex_box">
+            <div className="ct_box">
+              <div className="name_title">
+                고객명
+              </div>
+              <div className="name_text">
+                홍길동
+              </div>
+            </div>
+          </div>
         </div>
+        
+        
       </div>
     </React.Fragment>
   );
