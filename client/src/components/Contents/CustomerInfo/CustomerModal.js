3734d3426b3af5c86188b3db1cad24d53b6244e2
reviday <reviday@naver.com>
2020-06-24 17:26:31 +0900

    Update CustomerModal frame
    고객 테이블 인터페이스 정리 후 작업 예정

--- client/src/components/Contents/CustomerInfo/CustomerModal.js
+++ client/src/components/Contents/CustomerInfo/CustomerModal.js
@@ -1,6 +1,9 @@
 import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import moment from 'moment';
+
+import Alert from 'common/Modal/ModalAlert';
+import Confirm from 'common/Modal/ModalConfirm';
 import BorderButton from 'common/Button/BorderButton';
 import 'common/Modal/Modal.scss';
 
@@ -8,107 +11,117 @@ const ModalContents = (props) => {
   const [state, setState] = useState(props.data);
   const items = props.items;
 
+  // alertModal State
+  const [alertModal, setAlertModal] = useState({
+    show: false,
+    title: '',
+    content: ''
+  });
+
+  const toggleAlert = () => {
+    setAlertModal({
+      show: false,
+      title: '',
+      content: ''
+    });
+  };
+
+  // confirmModal State
+  const [confirmModal, setConfirmModal] = useState({
+    show: false,
+    title: '',
+    content: '',
+    type: ''
+  });
+
+  const toggleConfirm = () => {
+    setConfirmModal({
+      show: false,
+      title: '',
+      content: '',
+      type: ''
+    });
+  };
+
   const checkValidate = () => {
-    let validate = true;
+    let validation = true;
+    let message = '';
 
-    return true;
+
+    /**
+     * check validate
+     */
+
+
+    // validation에서 체크되지 않은 항목이 존재하면 alert창 출력
+    if (!validation) {
+      setAlertModal({
+        show: true,
+        title: '오류 메시지',
+        content: message
+      });
+    }
+
+    return validation;
   };
 
-  const insertCustomer = async () => {
+
+  // 일괄 처리 함수
+  const executeCustomer = async (type, id) => {
     // check validate
     if (checkValidate()) return false;
 
+    // data setting
+    let data = {
+      category: 'order',
+      action: '',
+      /**
+       * 고객 테이블 인터페이스 정리 후 작업.
+       */
+    };
+
+    switch (type) {
+
+      case 'insert':
+        data.action = 'i';
+        break;
+      case 'update':
+        data.id = id;
+        data.action = 'u';
+        break;
+      case 'delete':
+        data = {
+          category: 'order',
+          id: id,
+          action: 'd',
+        };
+        break;
+      case 'cancel':
+        // state값 초기화 후, modal 닫기
+        setState({});
+        props.hide();
+        break;
+      default: return false;
+
+    }
+
+    // set options
     let options = {
       url: `http://${Config.API_HOST.IP}:${Config.API_HOST.PORT}/api/order/making`,
       method: 'post',
-      data: {
-        category: 'order',
-        action: 'i',
-        site: state.site,
-        name: state.name,
-        telpno: state.telpno,
-        address: state.address,
-        needs: state.needs,
-        product: state.product,
-        price: state.price,
-        order_status: 0
-        // 날짜는 BACK-END 에서
-      }
+      data: data
     };
+
     try {
       console.log('options:::', options);
       let setData = await axios(options);
-      // if (setData.data.status === 400) {
-      //   setAlertModal({
-      //     show: true,
-      //     title: '오류 메시지',
-      //     content: setData.data.message
-      //   });
-      //   return false;
-      // }
+      console.log('setData:::', setData);
 
-      console.log(setData);
       let result = setData.data.data;
       console.log(result);
-
-
     } catch (e) {
       console.log('ERROR', e);
     }
-  };
-
-  const calcelOrder = () => {
-    // state값 초기화 후, modal 닫기
-    setState({});
-    props.hide();
-  };
-
-  const updateOrder = async () => {
-    // check validate
-    if (checkValidate()) return false;
-
-    let options = {
-      url: `http://${Config.API_HOST.IP}:${Config.API_HOST.PORT}/api/order/making`,
-      method: 'post',
-      data: {
-        category: 'order',
-        action: 'u',
-        site: state.site,
-        name: state.name,
-        telpno: state.telpno,
-        address: state.address,
-        needs: state.needs,
-        product: state.product,
-        price: state.price,
-        order_status: state.order_status,
-        // order_date: state.order_date,
-        // complete_date: state.complete_date
-      }
-    };
-    try {
-      console.log('options:::', options);
-      let setData = await axios(options);
-      // if (setData.data.status === 400) {
-      //   setAlertModal({
-      //     show: true,
-      //     title: '오류 메시지',
-      //     content: setData.data.message
-      //   });
-      //   return false;
-      // }
-
-      console.log(setData);
-      let result = setData.data.data;
-
-      console.log(result);
-      
-    } catch (e) {
-      console.log('ERROR', e);
-    }
-  };
-
-  const deleteOrder = () => {
-
   };
 
   return (
@@ -170,18 +183,29 @@ const ModalContents = (props) => {
                 </div>
                 <div className="rows-mb-20" style={{ justifyContent: 'center', textAlign: 'center' }}>
                   {
-                    items.type === 'insertOrder'
+                    items.type === 'insertCustomer'
                       ? (
                         <React.Fragment>
                           <BorderButton
                             addClass="insertBtn"
-                            onHandle={() => insertOrder()}
+                            onHandle={() => {
+                              // check validate
+                              if (!checkValidate()) return false;
+                              setConfirmModal({
+                                show: true,
+                                title: '확인 메시지',
+                                content: '고객을 등록하시겠습니까?',
+                                type: 'insert'
+                              });
+                            }}
                             name="등록"
+                            style={{ width: '80px' }}
                           />
                           <BorderButton
                             addClass="cancelBtn"
-                            onHandle={() => calcelOrder()}
+                            onHandle={() => executeOrder('cancel')}
                             name="취소"
+                            style={{ width: '80px' }}
                           />
                         </React.Fragment>
                       )
@@ -189,18 +213,41 @@ const ModalContents = (props) => {
                         <React.Fragment>
                           <BorderButton
                             addClass="updateBtn"
-                            onHandle={() => updateOrder()}
+                            onHandle={() => {
+                              // check validate
+                              if (!checkValidate()) return false;
+                              setConfirmModal({
+                                show: true,
+                                title: '확인 메시지',
+                                content: '고객 정보를 수정하시겠습니까?',
+                                type: 'update',
+                                id: state?.id
+                              });
+                            }}
                             name="수정"
+                            style={{ width: '80px' }}
                           />
                           <BorderButton
                             addClass="deleteBtn"
-                            onHandle={() => deleteOrder()}
+                            onHandle={() => {
+                              // check validate
+                              if (!checkValidate()) return false;
+                              setConfirmModal({
+                                show: true,
+                                title: '확인 메시지',
+                                content: '고객 정보를 삭제하시겠습니까?',
+                                type: 'delete',
+                                id: state?.id
+                              });
+                            }}
                             name="삭제"
+                            style={{ width: '80px' }}
                           />
                           <BorderButton
                             addClass="cancelBtn"
-                            onHandle={() => calcelOrder()}
+                            onHandle={() => executeOrder('cancel')}
                             name="취소"
+                            style={{ width: '80px' }}
                           />
                         </React.Fragment>
                       )
@@ -211,6 +258,19 @@ const ModalContents = (props) => {
           </div>
         </div>
       </div>
+      <Confirm
+        view={confirmModal.show}
+        title={confirmModal.title}
+        content={confirmModal.content}
+        hide={toggleConfirm}
+        execute={executeCustomer(confirmModal.type, confirmModal?.id)}
+      />
+      <Alert
+        view={alertModal.show}
+        title={alertModal.title}
+        content={alertModal.content}
+        hide={toggleAlert}
+      />
     </React.Fragment>
   );
 };
