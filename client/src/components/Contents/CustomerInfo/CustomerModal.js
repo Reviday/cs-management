7269431916c384a844592a202d0f825221a7cd08
reviday <reviday@naver.com>
2020-07-13 23:17:23 +0900

    Add Image Upload
    이미지 업로드 기능만 일단 추가.
    
    # 기존 문제점
    formData.append('custom_image', img);
    
    img 목록들을 한 번에 append 해서 보내려던 것이 문제였음.
    
    # 해결방법
    for (let i = 0; i < img.length; i++) {
      formData.append('custom_image', img[i]);
    }
    
    이와 같이 하나의 key값에 계속 append 해주어야 정상적으로 받을 수 있다.

--- client/src/components/Contents/CustomerInfo/CustomerModal.js
+++ client/src/components/Contents/CustomerInfo/CustomerModal.js
@@ -136,7 +136,6 @@ const ModalContents = (props) => {
         { key: 'zipcode', name: '우편번호' }, // input - API
         { key: 'address', name: '주소' }, // input - API
         { key: 'detail_addr', name: '상세주소' }, // input
-        { key: 'product', name: '상품명' }, // input
       ];
 
       for (let key in state) {
@@ -188,7 +187,9 @@ const ModalContents = (props) => {
     }
 
     const formData = new FormData();
-    formData.append('file', img);
+    for (let i = 0; i < img.length; i++) {
+      formData.append('custom_image', img[i]);
+    }
     for (let key in state) {
       if (Object.prototype.hasOwnProperty.call(state, key)) {
         formData.append(key, state[key]);
@@ -279,13 +280,14 @@ const ModalContents = (props) => {
   const uploadImg = (files = []) => {
     let fileList = [];
     if (files.length > 0) {
-      // for (let i in files) {
-      //   if (Object.prototype.hasOwnProperty.call(files, i)) {
-      //     fileList.push(files[i]);
-      //   }
-      // }
-      setImg(img.concat(files[0]));
+      for (let i in files) {
+        if (Object.prototype.hasOwnProperty.call(files, i)) {
+          fileList.push(files[i]);
+        }
+      }
     }
+    setImg(fileList);
+
   };
 
   return (
@@ -295,8 +297,9 @@ const ModalContents = (props) => {
           <div className="box_layout noshadow">
             <div className="_content">
               <div className="grid_box">
-                <div className="rows-mb-20">
-                  {
+                <form method="post" encType="multipart/form-data">
+                  <div className="rows-mb-20">
+                    {
                     // siteList가 존재하지 않거나, 개수가 0개이면
                     // input 스타일로 처리. 있으면 select 스타일로 처리.
                     siteList && siteList.length > 0
@@ -319,95 +322,95 @@ const ModalContents = (props) => {
                         />
                       )
                   }
-                </div>
-                <div className="rows-mb-20">
-                  <Input
-                    name="고객명"
-                    value={state.name}
-                    setValue={e => setState({ ...state, name: e })}
-                    style={{ width: '150px' }}
-                    disabled={items.type === 'showCustomer'}
-                  />
-                </div>
-                <div className="rows-mb-20">
-                  <Input
-                    name="연락처"
-                    value={state.telpno}
-                    setValue={e => setState({ ...state, telpno: e })}
-                    style={{ width: '300px' }}
-                    setReg={/[^0-9]/gi}
-                    disabled={items.type === 'showCustomer'}
-                  />
-                </div>
-                <div className="rows-mb-20">
-                  <InputCustom
-                    name="주소"
-                    topObj={{
-                      value: state.zipcode,
-                      setValue: e => setState({ ...state, zipcode: e }),
-                      placeholder: '우편번호',
-                      style: { width: '100px' },
-                      disabled: true
-                    }}
-                    middleObj={{
-                      value: state.address,
-                      setValue: e => setState({ ...state, address: e }),
-                      placeholder: '주소',
-                      style: { width: '300px', marginBottom: '7px' },
-                      disabled: true
-                    }}
-                    bottomObj={{
-                      value: state.detail_addr,
-                      setValue: e => setState({ ...state, detail_addr: e }),
-                      placeholder: '상세주소',
-                      style: { width: '300px' },
-                      disabled: items.type === 'showCustomer'
-                    }}
-                    btnObj={{
-                      addClass: 'addr_searchBtn',
-                      value: '주소 검색',
-                      onClick: viewPostcode,
-                      disabled: items.type === 'showCustomer'
-                    }}
-                  />
-                </div>
-                <div className="rows-mb-20">
-                  <Input
-                    name="고객선호 스타일"
-                    value={state.interest_style}
-                    setValue={e => setState({ ...state, interest_style: e })}
-                    style={{ width: '300px', marginTop: '7px' }}
-                    titleStyle={{ fontSize: '15px', lineHeight: '16px' }}
-                    disabled={items.type === 'showCustomer'}
-                  />
-                </div>
-                <div className="rows-mb-20">
-                  <Input
-                    name="고객선호 원단"
-                    value={state.interest_skin}
-                    setValue={e => setState({ ...state, interest_skin: e })}
-                    style={{ width: '300px', marginTop: '7px' }}
-                    titleStyle={{ fontSize: '15px', lineHeight: '16px' }}
-                    disabled={items.type === 'showCustomer'}
-                  />
-                </div>
-                <div className="rows-mb-20">
-                  <Input
-                    name="메모"
-                    value={state.memo}
-                    setValue={e => setState({ ...state, memo: e })}
-                    style={{ width: '300px' }}
-                    disabled={items.type === 'showCustomer'}
-                  />
-                </div>
-                <div className="rows-mb-20">
-                  {/* TO-DO: showCustomer에 대한 처리 필요 */}
-                  <div className="row_title">
-                    사진등록
                   </div>
-                  <div className="row_input" style={{ width: '360px' }}>
-                    <div className={`img_list${img.length > 0 ? ' on' : ''}`}>
-                      {
+                  <div className="rows-mb-20">
+                    <Input
+                      name="고객명"
+                      value={state.name}
+                      setValue={e => setState({ ...state, name: e })}
+                      style={{ width: '150px' }}
+                      disabled={items.type === 'showCustomer'}
+                    />
+                  </div>
+                  <div className="rows-mb-20">
+                    <Input
+                      name="연락처"
+                      value={state.telpno}
+                      setValue={e => setState({ ...state, telpno: e })}
+                      style={{ width: '300px' }}
+                      setReg={/[^0-9]/gi}
+                      disabled={items.type === 'showCustomer'}
+                    />
+                  </div>
+                  <div className="rows-mb-20">
+                    <InputCustom
+                      name="주소"
+                      topObj={{
+                        value: state.zipcode,
+                        setValue: e => setState({ ...state, zipcode: e }),
+                        placeholder: '우편번호',
+                        style: { width: '100px' },
+                        disabled: true
+                      }}
+                      middleObj={{
+                        value: state.address,
+                        setValue: e => setState({ ...state, address: e }),
+                        placeholder: '주소',
+                        style: { width: '300px', marginBottom: '7px' },
+                        disabled: true
+                      }}
+                      bottomObj={{
+                        value: state.detail_addr,
+                        setValue: e => setState({ ...state, detail_addr: e }),
+                        placeholder: '상세주소',
+                        style: { width: '300px' },
+                        disabled: items.type === 'showCustomer'
+                      }}
+                      btnObj={{
+                        addClass: 'addr_searchBtn',
+                        value: '주소 검색',
+                        onClick: viewPostcode,
+                        disabled: items.type === 'showCustomer'
+                      }}
+                    />
+                  </div>
+                  <div className="rows-mb-20">
+                    <Input
+                      name="고객선호 스타일"
+                      value={state.interest_style}
+                      setValue={e => setState({ ...state, interest_style: e })}
+                      style={{ width: '300px', marginTop: '7px' }}
+                      titleStyle={{ fontSize: '15px', lineHeight: '16px' }}
+                      disabled={items.type === 'showCustomer'}
+                    />
+                  </div>
+                  <div className="rows-mb-20">
+                    <Input
+                      name="고객선호 원단"
+                      value={state.interest_skin}
+                      setValue={e => setState({ ...state, interest_skin: e })}
+                      style={{ width: '300px', marginTop: '7px' }}
+                      titleStyle={{ fontSize: '15px', lineHeight: '16px' }}
+                      disabled={items.type === 'showCustomer'}
+                    />
+                  </div>
+                  <div className="rows-mb-20">
+                    <Input
+                      name="메모"
+                      value={state.memo}
+                      setValue={e => setState({ ...state, memo: e })}
+                      style={{ width: '300px' }}
+                      disabled={items.type === 'showCustomer'}
+                    />
+                  </div>
+                  <div className="rows-mb-20">
+                    {/* TO-DO: showCustomer에 대한 처리 필요 */}
+                    <div className="row_title">
+                    사진등록
+                    </div>
+                    <div className="row_input" style={{ width: '360px' }}>
+                      <div className={`img_list${img.length > 0 ? ' on' : ''}`}>
+                        {
                         img.map((item, index) => {
                           let key = `${index}_${item.name}`;
                           return (
@@ -426,8 +429,8 @@ const ModalContents = (props) => {
                           );
                         })
                       }
-                    </div>
-                    {
+                      </div>
+                      {
                       items.type !== 'showCustomer'
                         && (
                           <React.Fragment>
@@ -450,10 +453,10 @@ const ModalContents = (props) => {
                           </React.Fragment>
                         )
                     }
+                    </div>
                   </div>
-                </div>
-                <div className="rows" style={{ justifyContent: 'center', textAlign: 'center' }}>
-                  {
+                  <div className="rows" style={{ justifyContent: 'center', textAlign: 'center' }}>
+                    {
                     items.type === 'insertCustomer'
                       && (
                         <React.Fragment>
@@ -475,7 +478,7 @@ const ModalContents = (props) => {
                         </React.Fragment>
                       )
                   }
-                  {
+                    {
                     items.type === 'update'
                       && (
                         <React.Fragment>
@@ -510,13 +513,14 @@ const ModalContents = (props) => {
                         </React.Fragment>
                       )
                   }
-                  <BorderButton
-                    addClass="cancelBtn"
-                    onHandle={() => executeCustomer('cancel')}
-                    name="취소"
-                    style={{ width: '80px' }}
-                  />
-                </div>
+                    <BorderButton
+                      addClass="cancelBtn"
+                      onHandle={() => executeCustomer('cancel')}
+                      name="취소"
+                      style={{ width: '80px' }}
+                    />
+                  </div>
+                </form>
               </div>
             </div>
           </div>
